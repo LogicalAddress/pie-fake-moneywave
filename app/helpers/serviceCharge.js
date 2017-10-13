8bfa2c0542d560f22252b150ee503a1408b63026
DR <dretnan@logicaladdress.com>
2017-10-13 12:00:11 +0100

    fix the 2naira lost

--- app/helpers/serviceCharge.js
+++ app/helpers/serviceCharge.js
@@ -3,15 +3,18 @@ const round = (value, decimals = 0) => (
 )
 
 module.exports = (amount = 45, fee = 0) => {
-  amount = parseFloat(amount)
+  amount = round(parseFloat(amount))
   fee = parseFloat(fee)
   const mwPercentage = 0.014, mwBaseFee = 10
   let moneywaveCommission = (mwPercentage * amount) + mwBaseFee
   let merchantCommission = fee - moneywaveCommission
+
+  moneywaveCommission = round(moneywaveCommission)
+  merchantCommission = round(merchantCommission)
   
   return {
     amount: amount * 100,
-    moneywaveCommission: Math.floor(moneywaveCommission * 100),
+    moneywaveCommission: moneywaveCommission * 100,
     chargedFee: (moneywaveCommission + merchantCommission) * 100,
     merchantCommission: merchantCommission * 100,
     netDebitAmount: (amount - (moneywaveCommission + merchantCommission)) * 100
--- dist/helpers/serviceCharge.js
+++ dist/helpers/serviceCharge.js
@@ -9,16 +9,19 @@ module.exports = function () {
   var amount = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 45;
   var fee = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-  amount = parseFloat(amount);
+  amount = round(parseFloat(amount));
   fee = parseFloat(fee);
   var mwPercentage = 0.014,
       mwBaseFee = 10;
   var moneywaveCommission = mwPercentage * amount + mwBaseFee;
   var merchantCommission = fee - moneywaveCommission;
 
+  moneywaveCommission = round(moneywaveCommission);
+  merchantCommission = round(merchantCommission);
+
   return {
     amount: amount * 100,
-    moneywaveCommission: Math.floor(moneywaveCommission * 100),
+    moneywaveCommission: moneywaveCommission * 100,
     chargedFee: (moneywaveCommission + merchantCommission) * 100,
     merchantCommission: merchantCommission * 100,
     netDebitAmount: (amount - (moneywaveCommission + merchantCommission)) * 100
